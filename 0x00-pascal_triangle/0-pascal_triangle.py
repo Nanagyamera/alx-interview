d1bf2eef18f1dbd7a81ceec696dece3f9c9063c4
Nanagyamera <nanagyamera3@gmail.com>
2023-06-27 14:15:20 -0700

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,11 +5,14 @@ A function that defines the pascal's triangle
 
 def pascal_triangle(n):
     """
-    first checks if n is less than or equal to 0. If so, it returns an empty list
+    first checks if n is less than or equal to 0 and returns an empty list
     """
     if n <= 0:
         return []
 
+    """
+    If n is greater than 0, an initial row [1] is added to the triangle list
+    """
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
