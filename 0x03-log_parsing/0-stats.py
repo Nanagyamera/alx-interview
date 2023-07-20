02792a15a629c6d64ab8dd23bb8ee0fdcba0f57a
Nanagyamera <nanagyamera3@gmail.com>
2023-07-20 08:05:33 -0700

    script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,73 +2,48 @@
 """
 script that reads stdin line by line and computes metrics
 """
-import re
 import sys
+import re
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    log_pattern = re.compile(
-        r'^(?P<ip>\S+) - \[(?P<date>.*?)\] "GET \/projects\/260 HTTP\/1\.1" (?P<status_code>\d+) (?P<file_size>\d+)'
-    )
-    match = log_pattern.match(input_line)
-    if match:
-        return match.groupdict()
-    return None
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{}: {}'.format(status_code, num), flush=True)
+def output(log: dict) -> None:
+    """
+    function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
 
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    if line_info:
-        status_code = line_info.get('status_code')
-        if status_code in status_codes_stats:
-            status_codes_stats[status_code] += 1
-        total_file_size += int(line_info.get('file_size', 0))
-    return total_file_size
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-def run():
-    '''Starts the log parser.
-    '''
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
     try:
         for line in sys.stdin:
-            total_file_size = update_metrics(
-                line.strip(),
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
-    except KeyboardInterrupt:
-        print_statistics(total_file_size, status_codes_stats)
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-if __name__ == '__main__':
-    run()
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
