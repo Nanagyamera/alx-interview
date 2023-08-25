5d9f97debee9ee67bc0dc647e12b1abdc8f64f06
Nanagyamera <nanagyamera3@gmail.com>
2023-08-25 08:52:50 -0700

    Implemented dynamic programming solution for coin change problem.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,15 +10,15 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     # Initialize an array to store the minimum number of coins needed
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     # Calculate the minimum number of coins needed for each total
     for i in range(1, total + 1):
         for coin in coins:
             if i - coin >= 0:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-    
+
     return dp[total] if dp[total] != float('inf') else -1
