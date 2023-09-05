17bfa50eec25665926685ccde32cac0fefcf25c4
Nanagyamera <nanagyamera3@gmail.com>
2023-09-05 09:20:31 -0700

    Implemented isWinner function for the game with dynamic programming.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,25 +5,21 @@ Prime game module.
 
 
 def isWinner(x, nums):
-    """Determines the winner of a prime game session with `x` rounds.
-    """
     if x < 1 or not nums:
         return None
-    marias_wins, bens_wins = 0, 0
-    # generate primes with a limit of the maximum number in nums
-    n = max(nums)
-    primes = [True for _ in range(1, n + 1, 1)]
-    primes[0] = False
-    for i, is_prime in enumerate(primes, 1):
-        if i == 1 or not is_prime:
-            continue
-        for j in range(i + i, n + 1, i):
-            primes[j - 1] = False
-    # filter the number of primes less than n in nums for each round
-    for _, n in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
-        bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2 == 1
-    if marias_wins == bens_wins:
+
+    maria_wins, ben_wins = 0, 0
+
+    for n in nums:
+        # Determine the number of prime numbers in the range [1, n]
+        primes_count = sum(1 for i in range(2, n + 1) if all(i % j != 0 for j in range(2, int(i**0.5) + 1)))
+
+        # Update wins for Maria and Ben based on the count of prime numbers
+        if primes_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins == ben_wins:
         return None
-    return 'Maria' if marias_wins > bens_wins else 'Ben'
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
